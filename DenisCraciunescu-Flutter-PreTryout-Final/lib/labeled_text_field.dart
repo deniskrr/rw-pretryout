3242445cf43c22bb26a5a2ab607ba2db43ff9f4a
Denis Craciunescu <denis.craciunescu@nagarro.com>
2021-08-19 22:32:43 +0300

    First commit

---
+++ DenisCraciunescu-Flutter-PreTryout-Final/lib/labeled_text_field.dart
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2021 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify,
+ * merge, publish, distribute, sublicense, create a derivative work,
+ * and/or sell copies of the Software in any work that is designed,
+ * intended, or marketed for pedagogical or instructional purposes
+ * related to programming, coding, application development, or
+ * information technology. Permission for such use, copying,
+ * modification, merger, publication, distribution, sublicensing,
+ * creation of derivative works, or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks
+ * that are released under various Open-Source licenses. Use of
+ * those libraries and frameworks are governed by their own
+ * individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'colors.dart';
+
+class LabeledTextField extends StatelessWidget {
+  final String label;
+  final String hint;
+  final TextEditingController? controller;
+
+  const LabeledTextField({
+    Key? key,
+    required this.label,
+    required this.hint,
+    this.controller,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(label, style: Theme.of(context).textTheme.headline6),
+          const SizedBox(
+            height: 8,
+          ),
+          TextField(
+              controller: controller,
+              decoration: InputDecoration.collapsed(
+                hintText: hint,
+                hintStyle: Theme.of(context).textTheme.bodyText1!.copyWith(
+                      color: lightGrey,
+                    ),
+              ),
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              keyboardType: TextInputType.number,
+              style: Theme.of(context).textTheme.bodyText1)
+        ],
+      ),
+    );
+  }
+}
